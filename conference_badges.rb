1b299f195550d30a7362e43620c01288808d6435
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 17:56:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,12 +7,12 @@ def batch_badge_creator(array)
   array.collect {|guest| badge_maker(guest)}
 end
 
-def assign_rooms
-  
+def assign_rooms(array)
+  array.collect {|guest| "Hello, #{guest}! You'll be assigned to room #{array.index(guest)+1}"
 end
 
 def printer
   guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts batch_badge_creator(guets)
-  puts assign_rooms
+  puts assign_rooms(guests)
 end
